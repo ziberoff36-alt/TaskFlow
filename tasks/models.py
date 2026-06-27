918883008b03e6c90aaca6e6b33a8a5ab90721d8
Николай <Ziberoff36@gmail.com>
2026-06-27 14:11:00 +0300

    refactor Task model

--- tasks/models.py
+++ tasks/models.py
@@ -5,15 +5,16 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class Status(models.TextChoices):
-    NEW = 'new', 'Не начато'
-    PROGRESS = 'progress', 'В процессе'
-    DONE = 'done', 'Завершено'
-
 class Task(models.Model):
+    class Status(models.TextChoices):
+        NEW = 'new', 'Не начато'
+        PROGRESS = 'progress', 'В процессе'
+        DONE = 'done', 'Завершено'
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, default='')
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='tasks')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name='tasks')
     status = models.CharField(choices=Status, default=Status.NEW, max_length=10)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f'{self.title} - {self.get_status_display()}'
