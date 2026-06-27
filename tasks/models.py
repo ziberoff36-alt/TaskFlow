8776855cd3fcadac56568ddb3b2eb72db4abf76e
Николай <Ziberoff36@gmail.com>
2026-06-27 09:39:30 +0300

    Added model Category

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
     title = models.CharField(max_length=100)
     is_done = models.BooleanField(default=False)
