9fe98d9348307c2e71cd293c4a6b0a6b1ae8ba09
Николай <Ziberoff36@gmail.com>
2026-06-27 18:54:28 +0300

    Add authenticated task list view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from tasks.models import Task
 
-# Create your views here.
+
+class TaskListView(LoginRequiredMixin,ListView):
+    model = Task
+    template_name = 'tasks/task_list.html'
+    context_object_name = 'tasks'
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
