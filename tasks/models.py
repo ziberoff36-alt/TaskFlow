cd56670a1bfaf329419780c12901bb0ca9ff9a09
Николай <Ziberoff36@gmail.com>
2026-06-27 11:33:13 +0300

    Add Status TextChoices and refactor status and category fields in Task model

--- tasks/models.py
+++ tasks/models.py
@@ -5,6 +5,15 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Status(models.TextChoices):
+    NEW = 'new', 'Не начато'
+    PROGRESS = 'progress', 'В процессе'
+    DONE = 'done', 'Завершено'
+
 class Task(models.Model):
     title = models.CharField(max_length=100)
-    is_done = models.BooleanField(default=False)
+    description = models.TextField(blank=True, default='')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='tasks')
+    status = models.CharField(choices=Status, default=Status.NEW, max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
