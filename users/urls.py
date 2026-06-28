fd79021e49a1b40c0725be8fd591110a0a309c09
Николай <Ziberoff36@gmail.com>
2026-06-28 21:57:17 +0300

    add login named url to urlpatterns

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from users.views import RegisterView
-
+from users.views import RegisterView, LoginView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
