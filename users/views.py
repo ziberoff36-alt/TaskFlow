fe181958de4be015cb5e1bbf30860b74e95cab44
Николай <Ziberoff36@gmail.com>
2026-06-28 21:58:37 +0300

    create LoginView for user authentication

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 from django.views import View
 
@@ -12,6 +13,20 @@ class RegisterView(View):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('task_list')
+            return redirect('login')
         else:
             return render(request, 'users/register.html', {'form': form})
+
+class LoginView(View):
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, 'users/login.html', {'form': form})
+    def post(self, request):
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('task_list')
+        else:
+            return render(request, 'users/login.html', {'form': form})
+
