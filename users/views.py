ec1d08d3e84564597077e41a5d9acffb27b05d8a
Николай <Ziberoff36@gmail.com>
2026-06-28 21:08:47 +0300

    Create RegisterView for user registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'users/register.html', {'form': form})
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('task_list')
+        else:
+            return render(request, 'users/register.html', {'form': form})
