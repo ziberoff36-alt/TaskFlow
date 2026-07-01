ab8f0d3d41529bb4f244966f08a3bee03fbb1423
Николай <Ziberoff36@gmail.com>
2026-07-01 11:58:23 +0300

    add task create named url to tasks urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from tasks.views import TaskListView
+from tasks.views import TaskListView, TaskCreateView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task_list'),
+    path('create/', TaskCreateView.as_view(), name='task_create'),
 ]
