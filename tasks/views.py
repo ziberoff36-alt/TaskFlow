8e99c03f2839f1e8482c9e9e1e0d7e029d6d85e9
Николай <Ziberoff36@gmail.com>
2026-07-01 12:01:04 +0300

    create task create view in views

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+from tasks.forms import TaskForm
 from tasks.models import Task
 
 
@@ -9,3 +11,12 @@ class TaskListView(LoginRequiredMixin,ListView):
     context_object_name = 'tasks'
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
+
+class TaskCreateView(LoginRequiredMixin,CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task_list')
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
